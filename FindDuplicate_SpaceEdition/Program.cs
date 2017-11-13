8fab4064e0c21da82fa5bff557c66d2087a508bd
BotJeff <Jeffrey.E.Ferreiras@gmail.com>
2017-11-13 10:15:11 -0500

    new challenges

---
+++ FindDuplicate_SpaceEdition/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDuplicate_SpaceEdition
+{
+    class Program
+    {
+
+        /*
+         * Find a duplicate, Space Edition™.
+         * We have a list of integers, where:
+         * 1. The integers are in the range 1..n
+         * 2. The list has a length of n + 1
+         *
+         * It follows that our list has at least one integer which appears at least twice.
+         * But it may have several duplicates, and each duplicate may appear more than twice.
+         * Write a function which finds an integer that appears more than once in our list.
+         * (If there are multiple duplicates, you only need to find one of them.)
+         * We're going to run this function on our new, super-hip Macbook Pro With Retina Display™.
+         * Thing is, the damn thing came with the RAM soldered right to the motherboard, so we can't upgrade our RAM.
+         * So we need to optimize for space!
+         */
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 9, 8, 7, 1, 6, 15, 8};
+            int result = FindRepeat(arr);
+        }
+
+        private static int FindRepeat(int[] arr)
+        {
+            int floor = 1;
+            int ceiling = arr.Length - 1;
+
+            while (floor < ceiling)                                                      
+            {
+                int mid = floor + (ceiling - floor) / 2;
+                int lowerRangeFloor = floor;
+                int lowerRangeCeiling = mid;
+                int upperRangeFloor = mid + 1;
+                int upperRangeCeiling = ceiling;
+
+                int lowerRangeCount = 0;
+
+                foreach (int item in arr)
+                {
+                    if (item >= arr[lowerRangeFloor] && item <= arr[lowerRangeCeiling])
+                    {
+                        lowerRangeCount++;
+                    }
+                }
+
+                int distinctPossibleIntegers = lowerRangeCeiling - lowerRangeFloor + 1;
+
+                if(lowerRangeCount > distinctPossibleIntegers)
+                {
+                    floor = lowerRangeFloor;
+                    ceiling = lowerRangeCeiling;
+                }
+                else
+                {
+                    floor = upperRangeFloor;
+                    ceiling = upperRangeCeiling;
+                }
+            }
+
+            return floor;
+        }
+    }
+}
---
+++ MaximumRepeatedNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumRepeatedNumbers
+{
+    class Program
+    {
+        /*
+         * Suppose array has series of numbers, if user wants to find maximum repeated number in an array.
+         */
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 10, 20, 100, 100, 100, 10, 30, 20, 40, 50, 12, 14 };
+            
+            int repeated = FindMaxRepeat(a);
+            Console.WriteLine(repeated);
+            Console.Read();
+        }
+
+        private static int FindMaxRepeat(int[] arr)
+        {
+            int max = -1;
+
+            HashSet<int> seen = new HashSet<int>();
+
+            foreach(var num in arr)
+            {
+                if (seen.Contains(num))
+                {
+                    if(max < num)
+                    {
+                        max = num;
+                    }
+                }
+
+                seen.Add(num);
+            }
+
+            return max;
+        }
+    }
+}
