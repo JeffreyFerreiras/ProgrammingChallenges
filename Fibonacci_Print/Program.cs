c9b6a117e6ad5c4e4ed052380e042d13f69056fb
Jeffrey Ferreiras <Jeffrey.E.Ferreiras@gmail.com>
2017-11-14 15:09:13 -0500

    added fib

---
+++ Fibonacci_Print/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci_Print
+{
+    class Program
+    {
+        /*
+         * Write a function to calculate the nth number in the Fibonacci sequence.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fib(8));
+            Console.Read();
+        }
+
+        static int Fib(int current)
+        {
+            if (current < 2)
+                return current;
+
+            return Fib(current - 1) + Fib(current - 2);
+        }
+    }
+}
