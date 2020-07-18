017e6ff783b5b18dd8c44d448169a03af87123a7
Jeffrey Ferreiras <jeffrey.e.ferreiras@gmail.com>
2020-07-18 15:01:01 -0400

    added fb interview questions

---
+++ AlmostPalindrome/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace AlmostPalindrome
+{
+    class Program
+    {
+
+        /*
+1. Problem: string with lowercase, almost palindrom.
+Given a string S consisting of lowercase English characters determine if you can make it a palindrome by removing at most 1 character.
+*/
+
+// aba - True, abacc - False, abba - True
+
+/* 1. (palindrom, candidate)
+
+// aba , acba;
+// */
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            PrintExpected()
+
+            void PrintExpected(bool actual, string expected)
+            {
+                Console.WriteLine($"actual: {actual}\tresult");
+            }
+        }
+
+
+
+        private bool IsAlmostPalindrom(string candidate)
+        {
+
+            for (int i = 0, j = candidate.Length - 1; i != j; i++, j--)
+            {
+
+                bool isMatch = candidate[i] == candidate[j];
+
+                if (!isMatch)
+                {
+                    if (candidate[i] == candidate[j - 1])
+                    {
+                        j--;
+                    }
+                    else if (candidate[i + 1] == candidate[j])
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ ShortestPathOfMaze/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShortestPathOfMaze
+{
+    /*
+Return the Shortest Path of the Maze
+
+Inputs
+entrance -> 0 0 1 0 0 0 0
+            0 0 1 1 1 1 0
+            0 0 1 0 0 0 0
+            0 0 0 0 1 0 1
+            1 1 1 0 0 0 0 -> exit
+
+Solution
+entrance -> + + + + + + +
+            0 0 1 1 1 1 +
+            0 0 1 + + + +
+            0 0 1 + 1 0 1
+            1 1 1 + + + + -> exit
+
+entrance -> + + + + + + +
+            0 0 1 1 1 1 +
+            0 0 1 0 0 + +
+            0 0 1 0 1 + 1
+            1 1 1 0 0 + + -> exit
+This is shortest! return 13
+*/
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        //private static int GetShortestPathSteps(int[][] matrix)
+        //{
+        //    /// 1 ; i
+        //    int j = 0;
+
+        //    var visited = new Dictionary<int, int>();
+
+        //    for (int i = 0; i < matrix.Length; i++)
+        //    {
+        //        int left = (i - 1) > 0 ? i - 1 : -1;
+        //        int right = (i + 1) < matrix.Length ? (i + 1) : -1;
+                
+
+        //        while (matrix[i][j + 1] == 0)
+        //        {
+        //            j++;
+        //        }
+        //    }
+
+        //    return 0;
+        //}
+    }
+}
