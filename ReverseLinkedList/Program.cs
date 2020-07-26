a9c9bf1618fd8aa1919e20f4c418672fcaa89c09
Jeffrey Ferreiras <jeffrey.e.ferreiras@gmail.com>
2020-07-26 17:20:32 -0400

    a

--- ReverseLinkedList/Program.cs
+++ ReverseLinkedList/Program.cs
@@ -21,6 +21,88 @@ namespace ReverseLinkedList
             linked.AddLast("three");
 
             linked.ReverseLinkedList();
+
+
+            var testNode = PopulateTestCase();
+            
+            Console.WriteLine("before:\n");
+
+            Print(testNode);
+
+            testNode = ReverseLinkedListRecursive(testNode);
+
+            Console.WriteLine("after\n");
+
+            Print(testNode);
+
+            Console.ReadLine();
+
+        }
+
+        private static void Print(Node testNode)
+        {
+            do
+            {
+                Console.WriteLine(testNode?.Value);
+                testNode = testNode.Next;
+             } while (testNode != null);
+        }
+
+        private static Node PopulateTestCase()
+        {
+            var random = new Random();
+            var node = new Node(5);
+            var root = node;
+
+            for (int i = 0; i < 2; i++)
+            {
+                node.Next = new Node(random.Next(100));
+                node = node.Next;
+            }
+
+            return root;
+        }
+
+        // 1,2,3,4
+
+        private static Node ReverseLinkedListRecursive(Node root)
+        {
+            var reversed = Reversed(root);
+
+            root.Next = null;
+
+            return reversed;
+
+            Node Reversed(Node node)
+            {
+                if(node?.Next == null)
+                {
+                    return node;
+                }
+
+                var prev = node;
+
+                var stackNode = Reversed(node.Next);
+
+                if (stackNode != null)
+                {
+                    stackNode.Next = prev;
+                }
+
+                return stackNode;
+            }
+        }
+
+        public class Node
+        {
+            public int Value { get; set; }
+            
+            public Node Next { get; set; }
+
+            public Node(int value)
+            {
+                this.Value = value;
+            }
         }
     }
 }
