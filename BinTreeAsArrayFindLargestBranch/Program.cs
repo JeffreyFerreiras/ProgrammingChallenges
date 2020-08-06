1b86b0c8d5f8d73269ce6cadb6c7f6f071669b47
Jeffrey Ferreiras <jeffrey.e.ferreiras@gmail.com>
2020-08-06 10:57:50 -0400

    a

---
+++ BinTreeAsArrayFindLargestBranch/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BinTreeAsArrayFindLargestBranch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string Solution(long[] arr)
+        {
+            // Type your solution here
+
+            const string Left = "Left";
+            const string Right = "Right";
+            bool isSkipStep = false;
+            long sumLeft = 0, sumRight = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (isSkipStep)
+                {
+                    sumLeft += arr[i];
+                }
+                else
+                {
+                    sumRight += arr[i];
+                }
+
+                isSkipStep = !isSkipStep;
+            }
+
+            if(sumLeft > sumRight)
+            {
+                return Left;
+            }
+            else if(sumRight > sumLeft)
+            {
+                return Right;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
