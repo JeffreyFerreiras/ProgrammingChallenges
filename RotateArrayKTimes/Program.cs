0f0ff4541e0239e69c1d02978bcbdd8345625026
Jeffrey Ferreiras <jeffrey.e.ferreiras@gmail.com>
2020-08-22 15:21:49 -0400

    d

---
+++ RotateArrayKTimes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RotateArrayKTimes
+{
+    class Program
+    {
+        /*
+        Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Follow up:
+
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+Could you do it in-place with O(1) extra space? 
+
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public void Rotate(int[] nums, int k)
+        {
+            int r = k % nums.Length;
+
+            Array.Reverse(nums, 0, nums.Length);
+            Array.Reverse(nums, r, (nums.Length - r));
+            Array.Reverse(nums, 0, r);
+        }
+    }
+}
---
+++ SingleNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace SingleNumber
+{
+    class Program
+    {
+        // Find non-repeated number [2,2,1,1,3] ans: 3
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int SingleNumber(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return nums.First();
+            }
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == -1) continue;
+
+                int j = i + 1;
+
+                while (j < nums.Length - 1 && nums[j] != nums[i])
+                {
+                    j++;
+                }
+
+                if (nums[j] != nums[i])
+                {
+                    return nums[i];
+                }
+
+                nums[j] = -1;
+            }
+
+            return nums.Last();
+        }
+    }
+}
