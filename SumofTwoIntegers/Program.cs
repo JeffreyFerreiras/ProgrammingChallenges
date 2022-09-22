3096b082a5c5353c93ebb01f9fe72ab2b1599ed1
Jeffrey Ferreiras <jeffrey.ferreiras@paygencorp.com>
2022-09-22 17:10:42 -0400

    bits

---
+++ SumofTwoIntegers/Program.cs
@@ -0,0 +1,47 @@
+﻿// Given two integers a and b, return the sum of the two integers without using the operators +and -.
+// Example 1:
+
+// Input: a = 1, b = 2
+// Output: 3
+// Example 2:
+
+// Input: a = 2, b = 3
+// Output: 5
+
+
+
+// Constraints:
+
+// -1000 <= a, b <= 1000
+
+var result = GetSum(1, 2);
+Console.WriteLine($"1 + 2 :" + result);
+
+var result2 = GetSum(2, 3);
+Console.WriteLine($"2 + 3 :" + result2);
+
+static int GetSum(int a, int b)
+{
+    int result = 0,
+    // carry now contains common set bits of "a" and "b"
+    carry = a & b;
+
+    if (Convert.ToBoolean(carry))
+    {
+        // Sum of bits of "a" and "b" where at least one 
+        // of the bits is not set
+        result = a ^ b;
+
+        // carry is shifted by one so that adding it 
+        // to "a" gives the required sum
+        carry = carry << 1;
+
+        result = GetSum(carry, result);
+    }
+    else
+    {
+        result = a ^ b;
+    }
+
+    return result;
+}
