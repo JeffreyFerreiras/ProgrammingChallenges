429956320ae9464756e5d367815c2d76c6d4d7fb
Jeffrey Ferreiras <jeffrey.ferreiras@paygencorp.com>
2025-02-19 15:07:02 -0500

    feat: Add NumMatrix project with implementation for range sum queries in a 2D matrix

---
+++ NumMatrix/NumMatrix.cs
@@ -0,0 +1,51 @@
+// See https://aka.ms/new-console-template for more information
+public class NumMatrix2
+{
+    private readonly int[][] _matrix;
+    private readonly Dictionary<string, int> _memo = new();
+
+    public NumMatrix2 (int[][] matrix)
+    {
+        _matrix = matrix;
+
+        BuildMemo(0, 0, matrix.Length - 1, matrix[0].Length - 1);
+    }
+
+    public void BuildMemo(int row1, int col1, int row2, int col2)
+    {
+        if (row2 < row1 || col2 < col1) //overlap detected, exit
+        {
+            return;
+        }
+
+        var key = $"{row1},{col1},{row2},{col2}";
+
+        var sum = 0;
+        for (int i = row1; i <= row2; i++)
+        { //row
+            for (int j = col1; j <= col2; j++)
+            { //col
+                sum += _matrix[i][j];
+            }
+        }
+
+        _memo[key] = sum;
+
+        //BFS each direction
+        BuildMemo(row1 + 1, col1, row2, col2);
+        BuildMemo(row1, col1 + 1, row2, col2);
+        BuildMemo(row1, col1, row2 - 1, col2);
+        BuildMemo(row1, col1, row2, col2 - 1);
+    }
+
+    public int SumRegion(int row1, int col1, int row2, int col2)
+    {
+        return _memo[$"{row1},{col1},{row2},{col2}"];
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.SumRegion(row1,col1,row2,col2);
+ */
---
+++ NumMatrix/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[][] matrix = new int[][] {
+            new int[]{3, 0, 1, 4, 2},
+            new int[]{5, 6, 3, 2, 1},
+            new int[]{1, 2, 0, 1, 5},
+            new int[]{4, 1, 0, 1, 7},
+            new int[]{1, 0, 3, 0, 5}
+        };
+        var numMatrix = new NumMatrix(matrix);
+
+        TestSumRegion(numMatrix, 2, 1, 4, 3, 8);
+        TestSumRegion(numMatrix, 1, 1, 2, 2, 11);
+        TestSumRegion(numMatrix, 1, 2, 2, 4, 12);
+    }
+
+    static void TestSumRegion(NumMatrix numMatrix, int row1, int col1, int row2, int col2, int expected)
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        int result = numMatrix.SumRegion(row1, col1, row2, col2);
+        sw.Stop();
+
+        Console.WriteLine($"Method: SumRegion({row1}, {col1}, {row2}, {col2})");
+        Console.WriteLine($"Result: {result} ticks");
+        Console.WriteLine($"Expected: {expected} ticks");
+        Console.WriteLine($"Time: {sw.ElapsedTicks} ticks");
+        Console.WriteLine();
+    }
+}
+
+public class NumMatrix
+{
+    // holds the sum of all elements from the top-left corner of the matrix to the current element
+    private int[,] matrixSum;
+    
+    public NumMatrix(int[][] matrix)
+    {
+        if (matrix == null || matrix.Length == 0) return; // edge case
+
+        // add an extra row and column for the sum of the first row and column,
+        // so we don't have to check for edge cases
+        matrixSum = new int[matrix.Length + 1, matrix[0].Length + 1]; 
+
+        // fill the matrixSum array
+        // iterate over the matrix and calculate the sum of all elements
+        //  from the top-left corner to the current element
+        for (int row = 0; row < matrix.Length; row++)
+        {
+            for (int col = 0; col < matrix[0].Length; col++)
+            {
+                matrixSum[row + 1, col + 1] = matrixSum[row + 1, col] + matrixSum[row, col + 1] - matrixSum[row, col] + matrix[row][col];
+            }
+        }
+    }
+    
+    public int SumRegion(int row1, int col1, int row2, int col2)
+    {
+        return matrixSum[row2 + 1, col2 + 1] - matrixSum[row1, col2 + 1] - matrixSum[row2 + 1, col1] + matrixSum[row1, col1];
+    }
+}
