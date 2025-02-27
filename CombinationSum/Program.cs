e4cc1573d15baf64920b98895b20a5ed8e57455f
Jeffrey Ferreiras <jeffrey.ferreiras@paygencorp.com>
2025-02-27 13:22:02 -0500

    feat: Add Combination Sum project with implementation for finding unique combinations that sum to a target

---
+++ CombinationSum/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CombinationSum
+{
+    public class Program
+    {
+        /*
+         * Leetcode 39. Combination Sum
+         * 
+         * Given an array of distinct integers candidates and a target integer target,
+         * return a list of all unique combinations of candidates where the chosen numbers sum to target.
+         * You may return the combinations in any order.
+         * 
+         * The same number may be chosen from candidates an unlimited number of times. 
+         * Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+         * 
+         * It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+         * 
+         * Constraints:
+         * 1 <= candidates.length <= 30
+         * 1 <= candidates[i] <= 200
+         * All elements of candidates are distinct.
+         * 1 <= target <= 500
+         */
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Leetcode 39. Combination Sum");
+            Console.WriteLine("----------------------------");
+
+            RunTestCase(new int[] { 2, 3, 6, 7 }, 7, new List<IList<int>> 
+            { 
+                new List<int> { 2, 2, 3 }, 
+                new List<int> { 7 } 
+            });
+
+            RunTestCase(new int[] { 2, 3, 5 }, 8, new List<IList<int>> 
+            { 
+                new List<int> { 2, 2, 2, 2 }, 
+                new List<int> { 2, 3, 3 }, 
+                new List<int> { 3, 5 } 
+            });
+
+            RunTestCase(new int[] { 2 }, 1, new List<IList<int>> { });
+        }
+
+        private static void RunTestCase(int[] candidates, int target, IList<IList<int>> expected)
+        {
+            Console.WriteLine($"Test Case: candidates = [{string.Join(", ", candidates)}], target = {target}");
+            Console.WriteLine($"Expected: {FormatResult(expected)}");
+
+            var solution = new Solution();
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var result = solution.CombinationSum(candidates, target);
+            stopwatch.Stop();
+
+            Console.WriteLine($"Result: {FormatResult(result)}");
+            Console.WriteLine($"Time elapsed: {stopwatch.ElapsedMilliseconds} ms ({stopwatch.ElapsedTicks} ticks)");
+            Console.WriteLine();
+        }
+
+        private static string FormatResult(IList<IList<int>> result)
+        {
+            if (result.Count == 0) return "[]";
+
+            var formattedCombinations = result
+                .Select(combo => $"[{string.Join(", ", combo)}]")
+                .ToArray();
+
+            return $"[{string.Join(", ", formattedCombinations)}]";
+        }
+    }
+}
---
+++ CombinationSum/Solution.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace CombinationSum;
+public class Solution
+{
+    public IList<IList<int>> CombinationSum(int[] candidates, int target)
+    {
+        //[2,3,6,7]
+        List<IList<int>> result = new(target);
+        Combinations(new List<int>(), target, 0);
+        return result;
+
+        void Combinations(List<int> comb, int currentTarget, int index)
+        {
+            // base case
+            if (comb.Count > 0 && currentTarget == 0)
+            { // if 0 is reached then its a valid combination
+                result.Add(comb.ToArray());
+            }
+            else if (currentTarget > 0)
+            {
+                for (int i = index; i < candidates.Length; i++)
+                {
+                    if (candidates[i] > currentTarget) continue;
+                    comb.Add(candidates[i]);
+                    // reduce the target by the current elem,
+                    // dont increment index since the next loop 
+                    // deeper in the stack will increment it, 
+                    // but the same number can be reused towards the answer.
+                    Combinations(comb, currentTarget - candidates[i], i);
+                    comb.RemoveAt(comb.Count - 1); //backtrack
+                }
+            }
+        }
+    }
+}
