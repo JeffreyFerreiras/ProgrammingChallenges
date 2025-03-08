94352bee878de8d038172ce9b9709522e7fe0315
Jeffrey Ferreiras <jeffrey.ferreiras@paygencorp.com>
2025-03-08 18:56:11 -0500

    feat: Refactor Rotting Oranges solution to use BFS for optimal time calculation and simplify grid validation

--- RottingOranges/Solution.cs
+++ RottingOranges/Solution.cs
@@ -20,7 +20,8 @@ public class Solution
             return grid[0][0] == 1 ? -1 : 0;
         }
 
-        List<int> mins = [];
+        int mins = 0;
+        Queue<(int, int)> queue = [];
         HashSet<(int, int)> visited = [];
 
         for (int i = 0; i < grid.Length; i++)
@@ -29,66 +30,59 @@ public class Solution
             {
                 if (grid[i][j] == 2)
                 {
-                    var min = ExploreBFS(grid, i, j, visited);
-                    if(ValidateGrid(grid, visited)) {
-                        mins.Add(min);
-                    }
-                    visited.Clear();
+                    queue.Enqueue((i, j));
+                    visited.Add((i, j));
                 }
             }
         }
-
-        return mins.Count > 0 ? mins.Min() : -1;
-
-        static bool ValidateGrid(int[][] grid, HashSet<(int, int)> visited)
-        {
-            for (int i = 0; i < grid.Length; i++)
-            {
-                for (int j = 0; j < grid[0].Length; j++)
-                {
-                    if (grid[i][j] == 1 && !visited.Contains((i, j)))
-                    {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-    }
-
-    private int ExploreBFS(int[][] grid, int row, int col, HashSet<(int, int)> visited)
-    {
-        Queue<(int, int)> queue = [];
-        int mins = 0;
-        
-        queue.Enqueue((row, col));
 
         while (queue.Count > 0)
         {
-            (row, col) = queue.Dequeue();
-
-            foreach (var coord in coords)
+            int size = queue.Count; // Process all oranges at current time
+            bool rottenAny = false;
+            
+            // Process all oranges at the current minute
+            for (int i = 0; i < size; i++)
             {
-                var newRow = row + coord[0];
-                var newCol = col + coord[1];
-
-                if (visited.Contains((newRow, newCol))
-                || newRow < 0
-                || newCol < 0
-                || newRow > grid.Length -1
-                || newCol > grid[0].Length -1
-                || grid[newRow][newCol] != 1
-                )
+                var (row, col) = queue.Dequeue();
+                
+                foreach (var coord in coords)
                 {
-                    continue;
+                    var newRow = row + coord[0];
+                    var newCol = col + coord[1];
+
+                    if (visited.Contains((newRow, newCol))
+                    || newRow < 0
+                    || newCol < 0
+                    || newRow > grid.Length - 1
+                    || newCol > grid[0].Length - 1
+                    || grid[newRow][newCol] != 1
+                    )
+                    {
+                        continue;
+                    }
+
+                    queue.Enqueue((newRow, newCol));
+                    visited.Add((newRow, newCol));
+                    grid[newRow][newCol] = 2;
+                    rottenAny = true;
                 }
+            }
+            
+            // Only increment time if we rotted any oranges this minute
+            if (rottenAny)
+            {
+                mins++;
+            }
+        }
 
-                queue.Enqueue((newRow, newCol));
-                visited.Add((newRow, newCol));
-
-                if (visited.Contains((row, col)))
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[0].Length; j++)
+            {
+                if (grid[i][j] == 1)
                 {
-                    mins += 1;
+                    return -1;
                 }
             }
         }
