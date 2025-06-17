86a6c3af8981a02b4c3642e0c0b29dec1913f1d8
Jeffrey Ferreiras <jeffrey.ferreiras@paygencorp.com>
2025-06-17 13:40:48 -0400

    feat: Add reverse_string function to reserve space for mutable string

--- rust/reverse_string/src/solution.rs
+++ rust/reverse_string/src/solution.rs
@@ -1,4 +1,10 @@
 
+pub fn reverse_string(target: &mut String) -> Option<&String> {
+    target.reserve(0);
+    
+    Some(target)
+}
+
 pub fn reverse_string_str(target: &str) -> String {
     let mut char_vec: Vec<char> = target.chars().into_iter().collect();
     reverse_char_vec(&mut char_vec);
