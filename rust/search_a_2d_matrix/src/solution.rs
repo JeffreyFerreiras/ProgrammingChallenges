b35320c8a99cc0124242e6fb6f7b8bb92b279ddc
Jeffrey Ferreiras <jeffrey.e.ferreiras@gmail.com>
2025-09-29 01:48:10 -0400

    refactor: Remove large matrix test cases and improve binary search logic in 2D matrix solution

--- rust/search_a_2d_matrix/src/solution.rs
+++ rust/search_a_2d_matrix/src/solution.rs
@@ -2,33 +2,49 @@ pub struct Solution;
 
 impl Solution {
     pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
-        let mut low = 0;
-        let mut high = matrix.len() - 1;
-        let mut mid = 0;
+        if matrix.is_empty() || matrix[0].is_empty() {
+            return false;
+        }
+
+        let mut low = 0i32;
+        let mut high = (matrix.len() - 1) as i32;
+        let mut mid = 0i32;
         
-        while low < high {
+        while low <= high {
             mid = (low + high) / 2;
-            if matrix[mid][0] > target {
+            if matrix[mid as usize][0] > target {
+                if target >= matrix[mid as usize][0] 
+                    && target <= matrix[mid as usize][matrix[mid as usize].len() - 1] {
+                    break;
+                }
                 high = mid - 1;
-            } else if matrix[mid][0] < target {
-                low = mid + 1;
+            } else if matrix[mid as usize][matrix[mid as usize].len() - 1] < target {
+                if target >= matrix[mid as usize][0] 
+                    && target <= matrix[mid as usize][matrix[mid as usize].len() - 1] {
+                    break;
+                }
+                low = (mid + 1) as i32;
             } else {
-                return true;
+                break;
             }
         }
 
-        Self::search(matrix[mid].clone(), target)
+        Self::search(matrix[mid as usize].clone(), target)
     }
 
     fn search(nums: Vec<i32>, target: i32) -> bool {
-        let mut low = 0;
-        let mut high = nums.len() - 1;
+        if nums.len() == 0 {
+            return false;
+        }
 
-        while low < high {
+        let mut low = 0i32;
+        let mut high = (nums.len() - 1) as i32;
+
+        while low <= high {
             let mid = (low + high) / 2;
-            if nums[mid] > target {
+            if nums[mid as usize] > target {
                 high = mid - 1;
-            } else if nums[mid] < target {
+            } else if nums[mid as usize] < target {
                 low = mid + 1;
             } else {
                 return true;
