6f3698130f8e3fad847c103af7aada60c2b54285
Jeffrey Ferreiras <jeffrey.e.ferreiras@gmail.com>
2025-09-30 00:07:57 -0400

    feat: Implement binary search algorithm for Koko's Eating Speed calculation

--- rust/koko_eating_bananas/src/solution.rs
+++ rust/koko_eating_bananas/src/solution.rs
@@ -1,7 +1,22 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn min_eating_speed(_piles: Vec<i32>, _h: i32) -> i32 {
-        todo!("Binary search over the eating speed to satisfy the hour limit.");
+    pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
+           let mut left = 1;
+           let mut right = *piles.iter().max().unwrap();
+
+           while left < right {
+            let speed = left + (right - left) / 2;
+            let hours = piles.iter()
+             .map(|&pile| pile / speed + if pile % speed > 0 { 1 } else { 0 })
+             .sum::<i32>();
+
+            if hours <= h {
+                right = speed;
+            } else {
+                left = speed + 1;
+            }
+           }
+           left
     }
 }
