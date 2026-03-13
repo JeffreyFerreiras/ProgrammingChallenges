fff05cae0af60e494c43c46f2f9b890eeb1a48a9
Jeffrey Ferreiras <jeffrey.e.ferreiras@gmail.com>
2026-03-12 23:47:09 -0400

    feat: Add ImplementTrie project with initial TrieNode and PrefixTree classes

---
+++ csharp/ImplementTrie/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ImplementTrie;
+
+public class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Implement Trie (Prefix Tree)");
+		Console.WriteLine(new string('=', 30));
+
+		var prefixTree = new PrefixTree();
+
+		Console.WriteLine("Scaffold created.");
+		Console.WriteLine($"Instance type: {prefixTree.GetType().Name}");
+		Console.WriteLine("Methods are intentionally left unimplemented.");
+	}
+}
